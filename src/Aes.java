62e94004d25fb16f7286a04f8d5254a9434ab0ad
María José Cortés <maria2000jose.mj@gmail.com>
2019-09-27 15:51:00 -0600

    feat: AES Class
    Class only to encrypt & decrypt

--- src/Aes.java
+++ src/Aes.java
@@ -6,16 +6,9 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Base64;
 
-public class Encriptar {
-	private final String textoEncriptado = "xZwM7BWIpSjYyGFr9rhpEa+cYVtACW7yQKmyN6OYSCv0ZEg9jWbc6lKzzCxRSSIvOvlimQZBMZOYnOwiA9yy3YU8zk4abFSItoW6Wj0ufQ0=";
-	private StringBuilder llave = new StringBuilder("29dh120_dk1_3");
+public class Aes {
+    private static SecretKeySpec secretKey;
 	private static byte[] key;
-
-	public Encriptar() {
-
-	}
-
-	private static SecretKeySpec secretKey;
 
 	public static void setKey(String myKey) {
 		MessageDigest sha = null;
@@ -32,39 +25,23 @@ public class Encriptar {
 		}
 	}
 
-	public void getLlaveTanteo() {
-		String llaveTmp = "";
-		for (char letra = 'a'; letra != ('z' + 1); letra++) {
-			for (char numero = '0'; numero < ('9' + 1); numero++) {
-				llave.setCharAt(7, letra);
-				llave.setCharAt(11, numero);
-				llaveTmp = desencriptarAES(llave.toString());
-				if (llaveTmp != null) {
-					System.out.println(llaveTmp);
-				}
-			}
-		}
-	}
-
-	public String desencriptarAES(String secret) {
+	public String desencriptar(String secret, String textoEncriptado) {
 		try {
 			setKey(secret);
 			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
 			cipher.init(Cipher.DECRYPT_MODE, secretKey);
-			String texto = new String(cipher.doFinal(Base64.getDecoder().decode(textoEncriptado)));
-			System.out.println("Texto desencriptado:\n" + texto);
-			return secret;
+			return new String(cipher.doFinal(Base64.getDecoder().decode(textoEncriptado)));
 		} catch (Exception e) {
 			return null;
 		}
 	}
 
-	public String encriptarAES(String secret) {
+	public String encriptar(String secret, String texto) {
 		try {
 			setKey(secret);
 			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
 			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-			return Base64.getEncoder().encodeToString(cipher.doFinal(textoEncriptado.getBytes("UTF-8")));
+			return Base64.getEncoder().encodeToString(cipher.doFinal(texto.getBytes("UTF-8")));
 		} catch (Exception e) {
 			return null;
 		}
