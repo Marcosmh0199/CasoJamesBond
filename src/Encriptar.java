6cb45d73f1cba3275233265240742e2ce41ce625
Marcos Méndez Hidalgo <marcosmh0199@estudiantec.cr>
2019-09-27 12:40:31 -0600

    fix: Adding files for Eclipse project
    Some files of the eclispe project were removed by mistake

--- src/Encriptar.java
+++ src/Encriptar.java
@@ -7,76 +7,67 @@ import java.util.Arrays;
 import java.util.Base64;
 
 public class Encriptar {
-    private final String textoEncriptado = "xZwM7BWIpSjYyGFr9rhpEa+cYVtACW7yQKmyN6OYSCv0ZEg9jWbc6lKzzCxRSSIvOvlimQZBMZOYnOwiA9yy3YU8zk4abFSItoW6Wj0ufQ0=";
-    private StringBuilder llave = new StringBuilder("29dh120_dk1_3");
-    private static byte[] key;
+	private final String textoEncriptado = "xZwM7BWIpSjYyGFr9rhpEa+cYVtACW7yQKmyN6OYSCv0ZEg9jWbc6lKzzCxRSSIvOvlimQZBMZOYnOwiA9yy3YU8zk4abFSItoW6Wj0ufQ0=";
+	private StringBuilder llave = new StringBuilder("29dh120_dk1_3");
+	private static byte[] key;
 
-    public Encriptar(){
+	public Encriptar() {
 
-    }
+	}
 
-    private static SecretKeySpec secretKey;
-    public static void setKey(String myKey)
-    {
-        MessageDigest sha = null;
-        try {
-            key = myKey.getBytes("UTF-8");
-            sha = MessageDigest.getInstance("SHA-1");
-            key = sha.digest(key);
-            key = Arrays.copyOf(key, 16);
-            secretKey = new SecretKeySpec(key, "AES");
-        }
-        catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-    }
+	private static SecretKeySpec secretKey;
 
-    public void getLlaveTanteo(){
-        String llaveTmp = "";
-        for(char letra = 'a'; letra != ('z'+1); letra++){
-            for(char numero = '0'; numero < ('9'+1); numero++){
-                llave.setCharAt(7,letra);
-                llave.setCharAt(11,numero);
-                llaveTmp = desencriptarAES(llave.toString());
-                if(llaveTmp != null){
-                    System.out.println(llaveTmp);
-                }
-            }
-        }
-    }
+	public static void setKey(String myKey) {
+		MessageDigest sha = null;
+		try {
+			key = myKey.getBytes("UTF-8");
+			sha = MessageDigest.getInstance("SHA-1");
+			key = sha.digest(key);
+			key = Arrays.copyOf(key, 16);
+			secretKey = new SecretKeySpec(key, "AES");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public String desencriptarAES(String secret){
-        try
-        {
-            setKey(secret);
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            String texto = new String(cipher.doFinal(Base64.getDecoder().decode(textoEncriptado)));
-            System.out.println("Texto desencriptado:\n" + texto);
-            return secret;
-        }
-        catch (Exception e)
-        {
-            return null;
-        }
-    }
+	public void getLlaveTanteo() {
+		String llaveTmp = "";
+		for (char letra = 'a'; letra != ('z' + 1); letra++) {
+			for (char numero = '0'; numero < ('9' + 1); numero++) {
+				llave.setCharAt(7, letra);
+				llave.setCharAt(11, numero);
+				llaveTmp = desencriptarAES(llave.toString());
+				if (llaveTmp != null) {
+					System.out.println(llaveTmp);
+				}
+			}
+		}
+	}
 
-    public String encriptarAES(String secret)
-    {
-        try
-        {
-            setKey(secret);
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(textoEncriptado.getBytes("UTF-8")));
-        }
-        catch (Exception e)
-        {
-            return null;
-        }
+	public String desencriptarAES(String secret) {
+		try {
+			setKey(secret);
+			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+			cipher.init(Cipher.DECRYPT_MODE, secretKey);
+			String texto = new String(cipher.doFinal(Base64.getDecoder().decode(textoEncriptado)));
+			System.out.println("Texto desencriptado:\n" + texto);
+			return secret;
+		} catch (Exception e) {
+			return null;
+		}
+	}
 
-    }
+	public String encriptarAES(String secret) {
+		try {
+			setKey(secret);
+			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+			return Base64.getEncoder().encodeToString(cipher.doFinal(textoEncriptado.getBytes("UTF-8")));
+		} catch (Exception e) {
+			return null;
+		}
+
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
-    private static Encriptar encriptar;
-    public static void main(String[] args) {
-        encriptar = new Encriptar();
-        encriptar.getLlaveTanteo();
-    }
+	private static Encriptar encriptar;
+
+	public static void main(String[] args) {
+		encriptar = new Encriptar();
+		encriptar.getLlaveTanteo();
+	}
 }
